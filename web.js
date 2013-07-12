43bb80301a6cdec61e1ee948c33080f1b12a7682
Radheka G <radhekag@gmail.com>
2013-07-12 00:06:06 +0000

    Updated web.js 2

--- web.js
+++ web.js
@@ -2,11 +2,23 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var content;
+
+fs.readFile('./index.html', function read(err, data) {
+	if (err) {
+		throw err;
+	}
+	content = data;
+
+	console.log(content);
+});
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+B
 });
