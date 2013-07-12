f238232d63ff91bbc0a633394b4ed4d23ec3dfeb
Radheka G <radhekag@gmail.com>
2013-07-12 16:34:55 +0000

    Updated web.js 2

--- web.js
+++ web.js
@@ -4,19 +4,19 @@ var app = express.createServer(express.logger());
 
 var content;
 
-//fs.readFile('./index.html', function read(err, data) {
-//	if (err) {
-//		throw err;
-//	}
-//	content = data;
-//
-//	console.log(content);
+fs.readFile('./index.html', function read(err, data) {
+	if (err) {
+		throw err;
+	}
+	content = data;
+
+	console.log(content);
+
+});
 
 app.get('/', function(request, response) {
   response.send('Hi');
 });
-
-//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
