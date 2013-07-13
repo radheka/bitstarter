36252eaed2e297b956302f5e9d4219e04ae5dd3e
Radheka G <radhekag@gmail.com>
2013-07-13 02:09:01 +0000

    Changed web

--- web.js
+++ web.js
@@ -1,15 +1,13 @@
-var js = require('fs');
-
 var express = require('express');
+var fs = require('fs');
+var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8'));
 
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  var content = fs.readFileSync('./index.html', 'utf8');
-  response.send(content);
+app.get('/', function(request, response) { 
+response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function() {  
+console.log("Listening on " + port);
 });
