3e3daaa1ce9efe321caf8f3fa12bd73270a771f2
Radheka G <radhekag@gmail.com>
2013-07-13 00:19:11 +0000

    Updated web.js 2

--- web.js
+++ web.js
@@ -2,9 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var content = fs.readFileSync('./index.html');
-
 app.get('/', function(request, response) {
+  var content = fs.readFileSync('./index.html', 'utf8');
   response.send(content);
 });
 
